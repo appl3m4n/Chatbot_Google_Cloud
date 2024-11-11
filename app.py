3e38b7f4fe599e65427703929f0f821d5ec57de5
Roman Hrdlovic <109904342+appl3m4n@users.noreply.github.com>
2024-11-11 18:49:14 +0100

    new

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, url_for, redirect, request, flash
 from flask_mysqldb import MySQL
 import json
 import os
 import pymysql
 import openai
 from difflib import get_close_matches
+
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+from flask_mysqldb import MySQL
+import MySQLdb
 
 app = Flask(__name__)
 
@@ -31,6 +39,103 @@ print("Host:", db_name)
 print("Database Name:", db_connection_name)
 
 #mysql = MySQL(app)
+
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''  # Add your password if required
+app.config['MYSQL_DB'] = 'users_db'
+
+mysql = MySQL(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+class User(UserMixin):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute("SELECT * FROM login WHERE id = %s", (user_id,))
+    user_data = cur.fetchone()
+    cur.close()
+    if user_data:
+        return User(user_data['id'], user_data['username'], user_data['password'])
+    return None
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cur.execute("SELECT * FROM login WHERE username = %s", (username.data,))
+        existing_user_username = cur.fetchone()
+        cur.close()
+        if existing_user_username:
+            raise ValidationError('That username already exists. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Login')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cur.execute("SELECT * FROM login WHERE username = %s", (form.username.data,))
+        user_data = cur.fetchone()
+        cur.close()
+        if user_data and bcrypt.check_password_hash(user_data['password'], form.password.data):
+            user = User(user_data['id'], user_data['username'], user_data['password'])
+            login_user(user)
+            return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    print(f"Current User ID: {current_user.id}")  # Log the current user's ID
+    return render_template('dashboard.html', username=current_user.username)
+
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO login (username, password) VALUES (%s, %s)", (form.username.data, hashed_password))
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
 
 # Load knowledge base from JSON file
 def load_knowledge_base(file_path: str):
@@ -132,6 +237,7 @@ def submit():
         
     # Render the template with user input, model output, and user details
     return render_template('index_submit.html', chatgpt_input=chatgpt_input, chatgpt_output=chatgpt_output, userDetails=userDetails)
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
